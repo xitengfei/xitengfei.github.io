231fad229c31e4e4de594c1e339df44509a45aa9
tengfei2015 <xitengfei@aragoncs.com>
2016-11-14 22:46:24 +0800

    222

--- projects/qybm-inc/js/scripts.js
+++ projects/qybm-inc/js/scripts.js
@@ -1,43 +1,8 @@
 (function($){
 
-function runInViewPort(){
-	$("body > div.app").each(function(){
-		var that = this;
-		if(isScrolledIntoView2(this)){
-			setTimeout(function(){
-				$(that).find(".circle").addClass("open");
-			}, 300);
-		}
-	});
-
-	var $count = $("body > div.intro");
-	if(isScrolledIntoView2($count) && !xCounterRendered){
-		xCounterRendered = true;
-		//counter
-		setTimeout(function(){
-			$(".xcounter").each(function(i, e){
-				var counter = new xCounter({
-					obj: $(e),
-					color: '#FCA136'
-				});
-				counter.init();
-			});
-		}, 300);
-	}
-}
 
 function events(){
-	xCounterRendered = false;
 	
-	runInViewPort();
-	$(window).scroll(function(){
-		runInViewPort();
-	});
-	
-	$(".case-list li").mouseover(function(){
-		$(this).addClass("active");
-		$(this).siblings().removeClass("active");
-	});
 }
 
 
