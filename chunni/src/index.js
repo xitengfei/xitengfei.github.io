98bfde559ef805cf951374315a75b08f4bc29131
xitengfei <447360803@qq.com>
2019-04-09 22:39:11 +0800

    xx

--- chunni/src/index.js
+++ chunni/src/index.js
@@ -5,15 +5,4 @@ import './css/app.css';
 
 import Logo from './img/spring-tree.svg';
 
-function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-}
-  
-document.body.appendChild(component());
 document.querySelector(".logo img").src = Logo;
