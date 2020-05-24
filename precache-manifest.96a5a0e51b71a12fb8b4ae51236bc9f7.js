6544c800547bac24e833eebee91bdd562c43c3b8
xitengfei <tengfei.xi@qq.com>
2020-05-24 08:25:12 +0800

    Updates

---
+++ precache-manifest.96a5a0e51b71a12fb8b4ae51236bc9f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b264e8e03ad8622c9a4ef13a3c4d1d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c52de6ead83ff5573845",
+    "url": "/static/css/main.dee4941a.chunk.css"
+  },
+  {
+    "revision": "8c8b062eccfbc6d7fee7",
+    "url": "/static/js/2.6c5f1d99.chunk.js"
+  },
+  {
+    "revision": "c52de6ead83ff5573845",
+    "url": "/static/js/main.9f628214.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee21b2bee0a9d9ef9d724dd911b1f7fa",
+    "url": "/static/media/abcd.ee21b2be.jpg"
+  },
+  {
+    "revision": "04d46ad56507c04b1113f087200b0110",
+    "url": "/static/media/bg-card2.04d46ad5.png"
+  },
+  {
+    "revision": "ef5740eec34d7574f2ed854fb6a22407",
+    "url": "/static/media/bg1.ef5740ee.jpg"
+  },
+  {
+    "revision": "3c429a3a15ecfe4cee19f35e5e00f920",
+    "url": "/static/media/me3.3c429a3a.jpg"
+  }
+]);
