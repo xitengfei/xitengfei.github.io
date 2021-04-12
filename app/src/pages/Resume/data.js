41f2c88d8fa49cda7322b543664c56441bfddcb3
Tengfei Xi <xitengfei@github.com>
2021-04-13 07:07:48 +0800

    update

--- app/src/pages/Resume/data.js
+++ app/src/pages/Resume/data.js
@@ -1,133 +1,153 @@
 export default {
-    name: '郗腾飞',
-    title: '前端开发工程师',
-    experience: '6年',
-    avatar: require('@/pages/Resume/images/avatar1.jpg'),
-    breifDesc: `六年web前后端开发经验，精通ReactJs/es6，熟练掌握Html5/Css3响应式web开发，并能使⽤Webpack/Gulp
-    等⼯具进⾏前端构建。善于研究和探索，有较强解决问题的能⼒，对代码质量有较⾼的要求。`,
-    metaInfo: [
-        {title: '电话', value:'15652203404', icon: 'icon-telephone'},
-        {title: '邮箱', value:'tengfei.xi@qq.com', icon: 'icon-youxiang1'},
-        {title: '地址', value:'北京·昌平', icon: 'icon-location'},
-        {title: '个人主页', value:'http://xitengfei.github.io', icon: 'icon-github'}
-    ],
-    workExperience: [
-        {
-            period: '2018.12 — 今',
-            name: '⽤友网络科技股份有限公司',
-            role: '⾼级前端开发⼯程师',
-            description: `
-                <h5>开发智能分析产品，主要⼯作内容 </h5>
-                <ol>
-                    <li>根据业务需求开发核心功能模块</li>
-                    <li>解决项目中存在的各种问题，保障项目顺利上线</li>
-                    <li>整体把控产品质量，优化/重构代码，增强产品的稳定性和可维护性</li>
-                    <li>帮助团队中的前端伙伴，提供解决问题的思路</li>
-                    <li>协助架构师完成微前端微服务的拆分</li>
-                </ol>`,
-            achievement: `
-            <p>这是⼀个比较大的数据可视化产品，在这个项目的开发中，每个⼈都有⾃⼰负责的功能模块，在这种情况下锻
-            炼了协同⼯作的能⼒。同时对于⽐较复杂的功能，先从架构的⾓度进⾏设计，然后再代码实现。在⼯作中不断
-            尝试新的技能，提⾼了开发的效率, ⽐如对react⾼阶组件的运⽤，对es6各种新特性的运⽤。</p>
-            `,
-        },
-        {
-            period: '2015.3 — 2018.12',
-            name: '北京华美汉盛信息技术有限公司',
-            role: '前后端开发工程师',
-            description: `
-            <h5>服务于美国三大电音节品牌之一的Insomniac公司项目，担任项目的Technical lead，主要工作内容</h5>
-            <ol>
-                <li>前端公共组件开发</li>
-                <li>Wordpress主题和插件开发</li>
-                <li>对接AWS, Google, SalesForce, MailChimp等各种第三方服务</li>
-                <li>Code Review团队成员提交的代码, 部署上线，还有一些运维工作</li>
-                <li>研究和使用新的技术并用到项目中来</li>
-                <li>培养团队新成员</li>
-            </ol>
-            `,
-            achievement: `
-            <p>
-            在这份⼯作中，负责客户公司旗下各个不同的项目，技术⼲度得到很⼤拓展。前端⽅⾯有Gulp, Nodejs, 
-React, Angular以及Sass, canvas, svg等, 后端⽅⾯有Wordpress, Laravel, GoLang等。同时还获得了很多
-与第三⽅服务对接的经验，包括AWS, Google, SalesForce, MailChimp, 微信等。更重要的，英语⽔平得到了
-很⼤提升，能⽐较轻松地阅读英⽂⽂档，与客户⽤英⽂通过gmail和slack等进⾏交流。
-            </p>
-            `,
-        },
-        {
-            period: '2014.6 — 2015.02',
-            name: '莱客创新（北京）科技有限公司',
-            role: 'PHP工程师/组长',
-            description: `
-            <p>在该公司期间，担任后端开发工程师，使用PHP的Yii框架，主要负责移动端API和后台管理系统的开发，
-            数据库设计等⼯作，另外还有开发爬虫进行数据抓取，在开发过程中还用到了Mongodb，Memcache，队列，定时任务等技术
-            </p>`,
-        },
-    ],
-    projectExperience: [
-        {
-            name: '⽤友云智能分析项目',
-            company: '用友网络科技股份有限公司',
-            role: '⾼级前端⼯程师',
-            period: '2018.12 — 今',
-            description: `
-            这是⼀个报表分析类的项目，提供数据可视化等丰富的功能，在这个项目中目前负责筛选器等相关功能的开发，筛
-            选器是整个产品的数据交互纽带，在开发相关功能的同时需要和其它模块做好融合，需要更多地从架构的⾓度去设
-            计开发和解决问题。在开发这个产品的过程中，对于复杂的功能需求，不断尝试将相关的代码逻辑进⾏提取，抽取
-            出服务，⾏为，模型等，并运⽤react 的⾼阶组件对⼀些重复的代码进⾏封装。
-            `,
-        },
-        {
-            name: 'Intranet',
-            company: '北京华美汉盛信息技术有限公司',
-            role: '前后端⼯程师',
-            period: '2018.08 - 2018.12',
-            description: `
-            Intranet是客户公司内部使⽤的项目, 有数据表格, 账单审批, 还有⼀些业务上的辅助功能. 技术栈上后端是Go语⾔, 
-前端是React全家桶配合Ant Design和TypeScript , 同时还对接了Google, AWS等第三⽅服务, 包括Google联合登
-陆, Gmail发送邮件, AWS S3服务等. 接⼿这个项目的时候还没有Go语⾔的相关经验, ⼀边学习的同时⼀边做新的功
-能和修复Bug, 同时将⾃⼰的React技能运⽤到了项目中。 
-            `,
-        },
-        {
-            name: 'Meme Generator',
-            company: '北京华美汉盛信息技术有限公司',
-            role: '前后端⼯程师',
-            period: '2017.06 — 2017.06',
-            description: `这是⼀个利⽤canvas技术做的在线合成图⽚的应⽤，⽤户可以上传的⾃⼰的图⽚, 然后放⼤缩⼩或拖动，输⼊⾃定
-            义⽂字并改变⽂字样式，然后点击按钮将合成的图⽚分享到Facebook和Twitter，也可以直接在线下载。为减轻服
-            务器的压⼒，全部功能都是纯粹在浏览器端完成的，为实现分享功能, 图⽚也是直接从浏览器端先上传到S3存储服务
-            上，并对所有的JS和CSS代码使⽤前端构建⼯具gulp进⾏了合并和压缩。`,
-        },
-        {
-            name: 'Sound Cloud ⾳乐播放器',
-            company: '北京华美汉盛信息技术有限公司',
-            role: '前后端⼯程师',
-            period: '2016.04 — 2016.04',
-            description: `开发⼀个基于Sound Cloud Javascript API的⾳乐播放器插件，要安装在客户所有的Festival站点上，并且需要实
-            现切换⻚⾯时⾳乐播放不被打断的效果。解决⽅案是对全站开启了Ajax模式，从⽽实现切换⻚⾯⾳乐播放不暂停。
-            由于Ajax模式的开启产⽣了很多问题， 然后将相关的⼀问题逐个进⾏了修复。今年⼜对代码进⾏了重构，借鉴了
-            Angular的思想将js代码模块化，优化过后的代码结构更加清晰，也易于扩展和维护。`,
-        },
-    ],
-    education: [
-        {
-            name: '许昌学院',
-            company: '计算机科学与技术系',
-            role: '计算机科学与技术系 -- 本科',
-            period: '2010.09 — 2014.07',
-            description: '',
-        },
-    ],
-    icomments: [
-        '熟练掌握ReactJS和ES6, 对于JS⾯向对象开发有⾃⼰的⼼得',
-        '熟练掌握各种Web前端技术（HTML5/CSS/Javascript等)和跨浏览器、跨终端的开发',
-        '有良好的代码习惯，⼀直保持简介的代码⻛格，并时刻考虑代码的性能',
-        '有丰富的后台开发经验，熟练掌握Linux, Apache, Mysql, PHP',
-        '熟练使⽤Git / Svn 进⾏代码管理',
-        '掌握微信⼩程序的开发，有实际开发经验',
-        '有丰富的API接⼝开发经验和多种开放平台对接经验',
-        '有很好的英⽂读写能⼒，喜欢在stackoverflow找问题的答案',
-    ]
+  name: '郗腾飞',
+  title: '前端开发工程师',
+  experience: '6年',
+  avatar: require('@/pages/Resume/images/avatar1.jpg'),
+  breifDesc: `六年web前后端开发经验，熟练掌握ReactJs/es6/Typescript，熟练掌握Html5/Css3响应式web开发，有丰富的微前端实战经验，熟悉公共组件库的建设。善于研究和探索，有较强解决问题的能⼒，对代码质量和产品性能有较⾼的要求。`,
+  metaInfo: [
+    { title: '电话', value: '15652203404', icon: 'icon-telephone' },
+    { title: '邮箱', value: 'tengfei.xi@qq.com', icon: 'icon-youxiang1' },
+    { title: '地址', value: '北京·昌平', icon: 'icon-location' },
+    { title: '个人主页', value: 'http://xitengfei.github.io', icon: 'icon-github' }
+  ],
+  workExperience: [
+    {
+      period: '2020.7 — 今',
+      name: '广联达科技股份有限公司',
+      role: '高级软件开发工程师',
+      description: `
+      <h5>负责数据中台项目的研发工作</h5>
+      <ol>
+        <li>负责产品需求的设计、开发，并解决技术难题</li>
+        <li>完成了微前端升级改造</li>
+        <li>建立了前端公共组件库，并封装了一系列的公共组件，提高了项目的质量和开发效率</li>
+        <li>代码重构，增强产品的稳定性和可维护性</li>
+        <li>与产品经理协作优化用户体验</li>
+      </ol>`,
+    },
+    {
+      period: '2018.12 — 2020.7',
+      name: '⽤友网络科技股份有限公司',
+      role: '⾼级前端开发⼯程师',
+      description: `
+      <h5>开发智能分析产品，主要⼯作内容 </h5>
+      <ol>
+          <li>根据业务需求开发核心功能模块</li>
+          <li>解决项目中存在的各种问题，保障项目顺利上线</li>
+          <li>整体把控产品质量，优化/重构代码，增强产品的稳定性和可维护性</li>
+          <li>帮助团队中的前端伙伴，提供解决问题的思路</li>
+          <li>协助架构师完成微前端微服务的拆分</li>
+      </ol>`,
+    },
+    {
+      period: '2015.3 — 2018.12',
+      name: '北京华美汉盛信息技术有限公司',
+      role: '前后端开发工程师',
+      description: `
+        <h5>服务于美国三大电音节品牌之一的Insomniac公司，担任Technical lead，主要工作内容</h5>
+        <ol>
+            <li>前端公共组件开发</li>
+            <li>Wordpress主题和插件开发</li>
+            <li>对接AWS, Google, SalesForce, MailChimp等各种第三方服务</li>
+            <li>Code Review团队成员提交的代码, 部署上线，还有一些运维工作</li>
+            <li>研究和使用新的技术并用到项目中来</li>
+            <li>培养团队新成员</li>
+        </ol>
+        `,
+//       achievement: `
+//             <p>
+//             在这份⼯作中，负责客户公司旗下各个不同的项目，技术广度得较⼤拓展。前端⽅⾯有Gulp, Nodejs, 
+// React, Angular以及Sass, canvas, svg等, 后端⽅⾯有Wordpress, Laravel, GoLang等。同时还获得了很多
+// 与第三⽅服务对接的经验，包括AWS, Google, SalesForce, MailChimp, 微信等。更重要的，英语⽔平得到了
+// 很⼤提升，能⽐较轻松地阅读英⽂⽂档，与客户⽤英⽂通过gmail和slack等进⾏交流。
+//             </p>
+//             `,
+    },
+    {
+        period: '2014.6 — 2015.02',
+        name: '莱客创新（北京）科技有限公司',
+        role: 'PHP工程师/组长',
+        description: `
+        <p>在该公司期间，担任后端开发工程师，使用PHP的Yii框架，主要负责移动端API和后台管理系统的开发，
+        数据库设计等⼯作，另外还有开发爬虫进行数据抓取，在开发过程中还用到了Mongodb，Memcache，队列，定时任务等技术。
+        </p>`,
+    },
+  ],
+  projectExperience: [
+    {
+      name: '数据中台项目',
+      company: '广联达科技股份有限公司',
+      role: '⾼级软件开发⼯程师',
+      period: '2020.07 — 今',
+      description: `
+        这是一个由微前端和后端微服务组织起来的数据中台项目，来到这个项目后，我首先对微前端架构进行了调整，将
+        登陆校验等逻辑以及页面上的公共部分统一放在基座应用上，子应用可以只关注自身业务逻辑。后来又利用father+dumi
+        的技术建立了前端公共组件库(之前项目是使用git子模块来进行组件代码共用，有很多问题)。在改项目中，全面使用
+        函数式组件进行开发，同时配合typescript使用，再加上自定义hooks等，开发效率和项目质量得到进一步提高。
+        `,
+    },
+    {
+      name: '⽤友云智能分析项目',
+      company: '用友网络科技股份有限公司',
+      role: '⾼级前端⼯程师',
+      period: '2018.12 — 2020.07',
+      description: `
+        这是⼀个报表分析类的项目，提供数据可视化等丰富的功能，在这个项目中目前负责筛选器等相关功能的开发，筛
+        选器是整个产品的数据交互纽带，在开发相关功能的同时需要和其它模块做好融合，需要更多地从架构的⾓度去设
+        计开发和解决问题。在开发这个产品的过程中，对于复杂的功能需求，不断尝试将相关的代码逻辑进⾏提取，抽取
+        出服务，⾏为，模型等，并运⽤react 的⾼阶组件对⼀些重复的代码进⾏封装。
+      `,
+    },
+    {
+      name: 'Intranet',
+      company: '北京华美汉盛信息技术有限公司',
+      role: '前后端⼯程师',
+      period: '2018.08 - 2018.12',
+      description: `
+      Intranet是客户公司内部使⽤的项目, 有数据表格, 账单审批, 还有⼀些业务上的辅助功能. 技术栈上后端是Go语⾔, 
+      前端是React全家桶配合Ant Design和TypeScript , 同时还对接了Google, AWS等第三⽅服务, 
+      包括Google联合登陆, Gmail发送邮件, AWS S3服务等. 接⼿这个项目的时候还没有Go语⾔的相关经验,
+      ⼀边学习的同时⼀边做新的功能和修复Bug, 同时将⾃⼰的React技能运⽤到了项目中。`,
+    },
+    {
+      name: 'Meme Generator',
+      company: '北京华美汉盛信息技术有限公司',
+      role: '前后端⼯程师',
+      period: '2017.06 — 2017.06',
+      description: `
+      这是⼀个利⽤canvas技术做的在线合成图⽚的应⽤，⽤户可以上传的⾃⼰的图⽚, 然后放⼤缩⼩或拖动，输⼊⾃定
+      义⽂字并改变⽂字样式，然后点击按钮将合成的图⽚分享到Facebook和Twitter，也可以直接在线下载。为减轻服
+      务器的压⼒，全部功能都是纯粹在浏览器端完成的，为实现分享功能, 图⽚也是直接从浏览器端先上传到S3存储服务
+      上，并对所有的JS和CSS代码使⽤前端构建⼯具gulp进⾏了合并和压缩。`,
+    },
+    // {
+    //   name: 'Sound Cloud ⾳乐播放器',
+    //   company: '北京华美汉盛信息技术有限公司',
+    //   role: '前后端⼯程师',
+    //   period: '2016.04 — 2016.04',
+    //   description: `开发⼀个基于Sound Cloud Javascript API的⾳乐播放器插件，要安装在客户所有的Festival站点上，并且需要实
+    //         现切换⻚⾯时⾳乐播放不被打断的效果。解决⽅案是对全站开启了Ajax模式，从⽽实现切换⻚⾯⾳乐播放不暂停。
+    //         由于Ajax模式的开启产⽣了很多问题， 然后将相关的⼀问题逐个进⾏了修复。今年⼜对代码进⾏了重构，借鉴了
+    //         Angular的思想将js代码模块化，优化过后的代码结构更加清晰，也易于扩展和维护。`,
+    // },
+  ],
+  education: [
+    {
+      name: '许昌学院',
+      company: '计算机科学与技术系',
+      role: '计算机科学与技术系 -- 本科',
+      period: '2010.09 — 2014.07',
+      description: '',
+    },
+  ],
+  icomments: [
+    '熟练掌握ReactJS、ES6、TypeScript, 对于JS⾯向对象开发有⾃⼰的⼼得',
+    '熟练掌握各种Web前端技术（HTML5/CSS/Javascript等)和跨浏览器、跨终端的开发',
+    '有良好的代码习惯，⼀直保持简介的代码⻛格，并时刻考虑代码的性能',
+    '有丰富的后台开发经验，熟练掌握Linux, Apache, Mysql, PHP',
+    '熟练使⽤Git / Svn 进⾏代码管理',
+    '有丰富的API接⼝开发经验和多种开放平台对接经验',
+    '有微信⼩程序，Electron等开发经验',
+    '有很好的英⽂读写能⼒，喜欢在stackoverflow找问题的答案',
+  ]
 }
--- app/src/pages/Resume/index.js
+++ app/src/pages/Resume/index.js
@@ -1,64 +1,64 @@
 
 import React from 'react';
 import Data from './data';
-import {DataContext} from './contexts';
-import {Head, Section, Experience} from './components';
+import { DataContext } from './contexts';
+import { Head, Section, Experience } from './components';
 
 import './index.less';
 
-class Resume extends React.Component{
-    render(){
-        return(
-            <DataContext.Provider value={Data}>
-                <div className="resume-body">
-                    <Head />
-                    <Section 
-                        icon={<img src={require('./images/001.png')} alt="person" />}
-                        title={'自我简介'}
-                        className="breif-desc"
-                    >
-                        <p>
-                        {Data.breifDesc}
-                        </p>
-                    </Section>
-                    <Section
-                        icon={<img src={require('./images/002.png')} alt="experience" />}
-                        title={'工作经历'}
-                    >
-                        <Experience dataSource={Data.workExperience} mode={'complex'} />
-                    </Section>
-                    <Section
-                        icon={<img src={require('./images/003.png')} alt="experience" />}
-                        title={'教育背景'}
-                        className='education'
-                    >
-                        <Experience dataSource={Data.education} mode={'simple'} />
-                    </Section>
-                    <Section
-                        icon={<img src={require('./images/004.png')} alt="experience" />}
-                        title={'项目经验'}
-                    >
-                        <Experience dataSource={Data.projectExperience} />
-                    </Section>
-                    <Section
-                        icon={<img src={require('./images/005.png')} alt="experience" />}
-                        title={'⾃我评价'}
-                        className='icomments'
-                    >
-                        <ol>
-                            {
-                                Data.icomments.map((item, index)=>{
-                                    return (
-                                        <li key={index}>{index + 1}. {item}</li>
-                                    )
-                                })
-                            }
-                        </ol>
-                    </Section>
-                </div>
-            </DataContext.Provider>
-        )
-    }
+class Resume extends React.Component {
+  render() {
+    return (
+      <DataContext.Provider value={Data}>
+        <div className="resume-body">
+          <Head />
+          <Section
+            icon={<img src={require('./images/001.png')} alt="person" />}
+            title={'自我简介'}
+            className="breif-desc"
+          >
+            <p>
+              {Data.breifDesc}
+            </p>
+          </Section>
+          <Section
+            icon={<img src={require('./images/002.png')} alt="experience" />}
+            title={'工作经历'}
+          >
+            <Experience dataSource={Data.workExperience} mode={'complex'} />
+          </Section>
+          <Section
+            icon={<img src={require('./images/004.png')} alt="experience" />}
+            title={'项目经验'}
+          >
+            <Experience dataSource={Data.projectExperience} />
+          </Section>
+          <Section
+            icon={<img src={require('./images/003.png')} alt="experience" />}
+            title={'教育背景'}
+            className='education'
+          >
+            <Experience dataSource={Data.education} mode={'simple'} />
+          </Section>
+          <Section
+            icon={<img src={require('./images/005.png')} alt="experience" />}
+            title={'⾃我评价'}
+            className='icomments'
+          >
+            <ol>
+              {
+                Data.icomments.map((item, index) => {
+                  return (
+                    <li key={index}>{index + 1}. {item}</li>
+                  )
+                })
+              }
+            </ol>
+          </Section>
+        </div>
+      </DataContext.Provider>
+    )
+  }
 }
 
 export default Resume;
